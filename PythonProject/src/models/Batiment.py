8d936e7f639db474b1b46c8a7f5da866b0586c4c
Mounirb213 <6304873@bdeb.qc.ca>
2026-02-20 14:18:30 -0500

    Update Batiment.py

--- PythonProject/src/models/Batiment.py
+++ PythonProject/src/models/Batiment.py
@@ -1,5 +1,9 @@
 class Batiment:
-    def init(self,surface,nbOccupants,isolantType):
+    def init(self, surface, nbOccupants, tempInt, chauffageType, isolantType, chambres):
         self.surface = surface
         self.nbOccupants = nbOccupants
+        self.tempInt = tempInt
         self.isolantType = isolantType
+        self.chambres = chambres
+
+    "def calculerPerteChaleurTotal(self):"
