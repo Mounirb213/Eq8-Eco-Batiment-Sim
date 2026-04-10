e26982171fe151a6611e99e40d8c20ce807728f8
NowayB <nberthezene@yahoo.ca>
2026-04-10 15:38:50 -0400

    Debut signal Classes

---
+++ eco-bat-sim-godot/FlaskConnector.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name FlaskConnector
+
+signal simulation_reussie(reponse: Dictionary)
+signal simulation_erreur(message: String)
+signal serveur_disponible(ok: bool)
---
+++ eco-bat-sim-godot/ResultsPanel.gd
@@ -0,0 +1,7 @@
+extends Control
+class_name ResultsPanel
+
+@export var label_consommation: Label
+@export var label_cout_annuel: Label
+@export var label_economies: Label
+@export var label_temperature_exterieure: Label
